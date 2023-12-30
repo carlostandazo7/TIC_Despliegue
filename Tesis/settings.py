d4238151657c5d1e699eed535157d918a612f7e4
Carlos Tandazo <carlosatandazo@gmail.com>
2023-12-29 19:18:23 -0500

    whitenoise

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -19,8 +19,10 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Quick-start development settings - unsuitable for production
